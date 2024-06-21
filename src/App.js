4f2955114ef8d299044eb4fc5e36f89f14d0ce68
veeralakshmi <veerasp8@gmail.com>
2024-06-21 10:27:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import 'react-dropdown/style.css';
 import './App.css';
 
 function App() {
+
+  const [info, setInfo] = useState({});
+  const [input, setInput] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [options, setOptions] = useState([]);
+  const [output, setOutput] = useState(0);
+
+  useEffect(() => {
+    Axios.get(`https://api.exchangerate-api.com/v4/latest/${from}`)
+      .then((res) => {
+        setInfo(res.data.rates);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [from]);
+
+  useEffect(() => {
+    setOptions(Object.keys(info));
+    if (Object.keys(info).length > 0) {
+      convert();
+    }
+  }, [info]);
+  
+  function convert() {
+    const rate = info[to];
+    if (rate) {
+      setOutput(input * rate);
+    }
+  }
+
+  function flip() {
+    const temp = from;
+    setFrom(to);
+    setTo(temp);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='heading'>
+        <h1>Currency Converter</h1>
+      </div>
+
+      <div className='container'>
+        <div className='left'>
+          <h3>Amount</h3>
+          <input
+            type="text"
+            placeholder='Enter the amount'
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+
+        <div className='middle'>
+          <h3>From</h3>
+          <Dropdown
+            options={options}
+            onChange={(e) => setFrom(e.value)}
+            value={from}
+            placeholder="From"
+          />
+        </div>
+
+        <div className='switch'>
+          <HiSwitchHorizontal size="30px" onClick={() => { flip() }} />
+        </div>
+
+        <div className="right">
+          <h3>To</h3>
+          <Dropdown
+            options={options}
+            onChange={(e) => setTo(e.value)}
+            value={to}
+            placeholder="To"
+          />
+        </div>
+      </div>
+
+      <div className='result'>
+        <button onClick={() => { convert() }}>Convert</button>
+        <h2>Converted Amount:</h2>
+        <p>{input + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+      </div>
     </div>
   );
 }
